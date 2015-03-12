f9295311e1856a1fd5984ec16703a1f52fbab263
Sebastian Munro <sebastian.munro@gmail.com>
2015-03-11 23:02:29 -0700

    Test cases added for foodtruck name, id, longitude, latitude, and size of list of FoodTrucks.

--- FoodVendorTracker/test/TestFoodParser.java
+++ FoodVendorTracker/test/TestFoodParser.java
@@ -1,4 +1,4 @@
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -11,11 +11,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.xml.sax.InputSource;
 
+import com.google.appengine.tools.util.Parser;
 import com.google.gwt.foodvendortracker.server.FoodTruckParser;
 import com.google.gwt.foodvendortracker.server.FoodTruckParserHandler;
 import com.google.gwt.foodvendortracker.shared.FoodTruck;
-
-
 
 public class TestFoodParser {
 
@@ -30,6 +29,69 @@ public class TestFoodParser {
 	@Test
 	public void testGetId() {
 		assertEquals("C1", reg.get(0).getId());
+	}
+	
+	@Test
+	public void testFoodTruckSize()
+	{
+		parser.parse();
+		List<FoodTruck> list = parser.getFoodTruckList();
+		assertEquals(111, list.size());
+	}
+	
+	@Test
+	public void testFoodTruckName()
+	{
+		parser.parse();
+		List<FoodTruck> list = parser.getFoodTruckList();
+		for(FoodTruck t: list)
+		{
+			assertNotNull(t.getName());
+		}
+	}
+
+	@Test
+	public void testFoodTruckDesciption()
+	{
+		parser.parse();
+		List<FoodTruck> list = parser.getFoodTruckList();
+		for(FoodTruck t: list)
+		{
+			assertNotNull(t.getDescription());
+		}
+	}
+	
+	@Test
+	public void testFoodTruckLatitude()
+	{
+		parser.parse();
+		List<FoodTruck> list = parser.getFoodTruckList();
+		for(FoodTruck t: list)
+		{
+			assertNotNull(t.getLatitude());
+		}
+	}
+	
+	@Test
+	public void testFoodTruckLongitude()
+	{
+		parser.parse();
+		List<FoodTruck> list = parser.getFoodTruckList();
+		for(FoodTruck t: list)
+		{
+			assertNotNull(t.getLongitude());
+		}
+	}
+	
+	@Test
+	public void testFoodTruckID()
+	{
+		parser.parse();
+		List<FoodTruck> list = parser.getFoodTruckList();
+		for(FoodTruck t: list)
+		{
+			assertNotNull(t.getId());
+		}
 	}
 
 }
