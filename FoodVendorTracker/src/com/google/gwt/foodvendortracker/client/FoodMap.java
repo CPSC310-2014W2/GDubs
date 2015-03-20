b4106a76f3db25426b370f53a2cd439fa82b1b69
Sebastian Munro <sebastian.munro@gmail.com>
2015-03-19 20:00:31 -0700

    Added flex table with search bar + cleaned some code up.

--- FoodVendorTracker/src/com/google/gwt/foodvendortracker/client/FoodMap.java
+++ FoodVendorTracker/src/com/google/gwt/foodvendortracker/client/FoodMap.java
@@ -6,6 +6,11 @@ import java.util.List;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyDownEvent;
+import com.google.gwt.event.dom.client.KeyDownHandler;
 import com.google.gwt.foodvendortracker.shared.FoodTruck;
 import com.google.gwt.maps.client.InfoWindowContent;
 import com.google.gwt.maps.client.MapWidget;
@@ -15,17 +20,28 @@ import com.google.gwt.maps.client.event.MapClickHandler;
 import com.google.gwt.maps.client.geom.LatLng;
 import com.google.gwt.maps.client.overlay.Marker;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DockLayoutPanel;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.RootPanel;
-
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Label;
 
 public class FoodMap implements EntryPoint {
 
-	
-	private static final String API_KEY     = "AIzaSyBDGcnhtpy_BVkfa82aOb_mSPZezrQRiWs"			; 
-	private ArrayList<LatLng> coordinates = 		new ArrayList<LatLng>()						;
-	private final FoodTruckServiceAsync foodTruckService = GWT.create(FoodTruckService.class)	;
-
+	private static final String API_KEY                 	 = 		"AIzaSyBDGcnhtpy_BVkfa82aOb_mSPZezrQRiWs"	; 
+	private ArrayList<LatLng> coordinates					 = 		new ArrayList<LatLng>()						;
+	private final FoodTruckServiceAsync foodTruckService	 = 		GWT.create(FoodTruckService.class)			;
+	private VerticalPanel foodTruckPanel 					 = 		new VerticalPanel()							;
+	private FlexTable foodTruckFlexTable 			  		 = 		new FlexTable()								;
+	private HorizontalPanel addPanel 				 		 = 		new HorizontalPanel()						;
+	private TextBox searchedFoodTruck 	 					 = 		new TextBox()								;
+	private Button searchButton 							 = 		new Button("Search")						;
+	private Label lastUpdatedLabel 							 = 		new Label()									;
+	  
+	  
 	 public void onModuleLoad()
 	  {
 		 loadFoodTruck();
@@ -43,10 +59,58 @@ public class FoodMap implements EntryPoint {
 	}
 	private void buildUi() 
 	{
-		showDisplay();		
+		loadMap();	
+		
+	    foodTruckFlexTable.setText(0, 0, "Name")				;		
+	    foodTruckFlexTable.setText(0, 1, "Rating")				;
+	    foodTruckFlexTable.setText(0, 2, "Favorite")			;
+	    foodTruckFlexTable.setText(0, 3, "Description")			;
+	    addPanel.add(searchedFoodTruck)							;
+	    addPanel.add(searchButton)								;
+	    foodTruckPanel.add(addPanel)							;
+	    foodTruckPanel.add(foodTruckFlexTable)					;
+	    foodTruckPanel.add(lastUpdatedLabel)					;
+	    RootPanel.get("nameFieldContainer").add(foodTruckPanel)	;
+	    searchedFoodTruck.setFocus(true)						;
+	    
+	    searchButton.addClickHandler(new ClickHandler() {
+	        public void onClick(ClickEvent event) {
+	          addFoodTruck()	;
+	        }
+	    });
+	    
+	    searchedFoodTruck.addKeyDownHandler(new KeyDownHandler() {
+	        public void onKeyDown(KeyDownEvent event) 
+	        {
+	          if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER)
+	          {
+	            addFoodTruck()	;
+	          }
+	       }
+	    });  
+	}
+	
+	private void addFoodTruck()
+	{
+		final String searchQuery = searchedFoodTruck.getText().toUpperCase().trim();
+		searchedFoodTruck.setFocus(true);
+		
+		// TODO auto complete searchQuery??
+	    // TODO Add a button to favorite this foodtruck in the table.
+	    // TODO Get the food trucks name, description etc.
+		// TODO only plot valid food trucks on the map (call function to update map)
+		
+	    // TODO add valid food trucks for food item entered..?
+		int row = foodTruckFlexTable.getRowCount()			;
+		foodTruckFlexTable.setText(row, 0, searchQuery)		;
+	    
+		
+		
+		searchedFoodTruck.setText("");
 	}
 
-	private void showDisplay() 
+
+	private void loadMap() 
 	{
 		foodTruckService.getFoodTrucks(new AsyncCallback<List<FoodTruck>>()
 		{
@@ -63,7 +127,7 @@ public class FoodMap implements EntryPoint {
 				double vanLng = -123.117533501725;
 				LatLng Vancouver 		= 		LatLng.newInstance(vanLat, vanLng)		;
 				final MapWidget map 	= 		new MapWidget(Vancouver, 17)			;
-				map.setSize("100%", "100%")			;
+				map.setSize("100%", "100%")				;
 				map.addControl(new LargeMapControl())	;
 
 				for(FoodTruck tr : result)
@@ -114,12 +178,14 @@ public class FoodMap implements EntryPoint {
 							}
 							counter ++;
 						}
-						String description = result.get(counter).getDescription();
-						String name = result.get(counter).getName();
+						String description = result.get(counter).getDescription()	;
+						String name = result.get(counter).getName()					;
+						
 						if(success == true)
 						{		
 							map.getInfoWindow().open(mark_coords,
 									new InfoWindowContent(name +  "<br />" + description));
+							
 						}						
 					}
 				});	
@@ -127,3 +193,15 @@ public class FoodMap implements EntryPoint {
 		});
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
